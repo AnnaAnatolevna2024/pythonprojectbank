ff0507aad55cad2b9e6aeebd451f8642c271a218
Anna <saa.omega@mail.ru>
2024-09-05 18:56:21 +0700

    файл с финальными изменениями и доработками

--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,23 @@
-from datetime import datetime
-import masks
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(data_card: str) -> str:
-    number_card = ''.join(el if el.isdigit() else '' for el in data_card)
-    number_card_mask = masks.get_mask_card_number(number_card)
-    name_card = ''.join("" if el.isdigit() else el for el in data_card)
+    """Функция, которая умеет обрабатывать информацию как о картах, так и о счетах"""
+
+    number_card = "".join(el if el.isdigit() else "" for el in data_card)
+    number_card_mask = get_mask_card_number(number_card)
+    name_card = "".join("" if el.isdigit() else el for el in data_card)
     data_card_mask = name_card + number_card_mask
     return data_card_mask
 
-print(mask_account_card('Visa Platinum 7000792289606361'))
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
 
 
 def get_date(old_date: str) -> str:
     """Функция принимает на вход строку с датой в формате '2024-03-11T02:26:18.671407' и
     возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
+
     slice_date = old_date[:10]
     date_new = ""
     for i in range(len(slice_date)):
@@ -26,5 +30,5 @@ def get_date(old_date: str) -> str:
     result = ".".join(split_date)
     return result
 
-print(get_date("11.03.2024"))
 
+print(get_date("11.03.2024"))
